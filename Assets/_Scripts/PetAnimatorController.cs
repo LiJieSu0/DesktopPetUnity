d6bfb67056fbeed77a056e7c47433cc299d9c4a9
LiJie Su <opwinopwin@gmail.com>
2025-05-12 00:58:37 +0800

    add animator and animations

--- Assets/_Scripts/PetAnimatorController.cs
+++ Assets/_Scripts/PetAnimatorController.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class PetAnimatorController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator animator;
+    bool facingRight = true;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("isSlide",true);
+        }
+        if(Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool("isSlide",false);
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            animator.Play("penguin_atack");
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;  // 翻轉 X 軸
+        transform.localScale = scale;
     }
 }
